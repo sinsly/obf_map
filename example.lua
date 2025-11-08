ccffa8658d3aacd1e1adfaf3f0c980d0d35d4430
sins <104468192+sinsly@users.noreply.github.com>
2025-11-07 23:17:29 -0500

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,2 +1,3 @@
 local src = loadstring(game:HttpGet("https://raw.githubusercontent.com/sinsly/m40fuscation/main/test.lua"))()
-local dcm = loadstring(game:HttpGet("https://raw.githubusercontent.com/sinsly/m40fuscation/main/connect.lua"))()
+local connect_fn = loadstring(game:HttpGet("https://raw.githubusercontent.com/sinsly/m40fuscation/main/connect.lua"))
+local dcm = connect_fn()
